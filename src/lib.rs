bfdc1386665f6f9ef7a4fd9fe1fa17eafbd0f9b2
Anton V <61997468+r3bu1ld3r@users.noreply.github.com>
2022-10-11 21:00:37 +0400

    improve flexibility of solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
+mod hook;
 mod log;
 
 use anyhow::Result;
 use ctor::{ctor, dtor};
 use frida_gum::{interceptor::Interceptor, Gum, Module, NativePointer};
+use hook::HookStorage;
 use lazy_static::lazy_static;
 use libc::{c_char, c_int, c_void};
 use log::HookLogger;
 use once_cell::sync::OnceCell;
+
+use crate::hook::Hook;
 
 type LibcOpen = unsafe extern "C" fn(*const c_char, flags: c_int) -> c_int;
 
@@ -26,21 +30,10 @@ unsafe extern "C" fn open_hook(name: *const c_char, flags: c_int) -> c_int {
         .unwrap_or(-1)
 }
 
-fn set_hook() -> Result<()> {
-    let mut interceptor = Interceptor::obtain(&GUM);
-    let libc_open = Module::find_export_by_name(None, "open").ok_or(anyhow::anyhow!(
-        "Can'f find open function in export symbols"
-    ))?;
-    let ptr = interceptor.replace(
-        libc_open,
-        NativePointer(open_hook as *mut c_void),
-        NativePointer(std::ptr::null_mut()),
-    )?;
-    unsafe {
-        //SAFETY
-        // 1. Rust LibcOpen type have the same signature as C open function
-        // 2. once cell is initialized here, so unwrap won't panic
-        ORIGINAL_OPEN.set(std::mem::transmute(ptr.0)).unwrap()
+fn set_hook(storage: &mut HookStorage) -> Result<()> {
+    let hook_list = vec![];
+    for (name, fptr) in hook_list {
+        storage.add(name, fptr)
     }
     eprintln!("[+] Hook successfully installed");
     Ok(())
@@ -48,13 +41,14 @@ fn set_hook() -> Result<()> {
 
 #[ctor]
 fn init() {
+    let mut storage = HookStorage::new();
     match HookLogger::new("./log.txt".to_string()) {
         Ok(logger) => {
             if let Err(_) = LOGGER.set(logger) {
                 eprintln!("[-] Can't initialize global hook logger")
             }
             eprintln!("[+] Logger created");
-            if let Err(e) = set_hook() {
+            if let Err(e) = set_hook(&mut storage) {
                 eprintln!("[-] Error while hook installation: {e}")
             }
         }
